8ac73368a1b566c705995ed55392d899b3096e3e
ezhov93 <ezhov93@users.noreply.github.com>
2020-05-14 15:07:09 +0300

    Create Global.h

---
+++ firmware/system/libraries/Global.h
@@ -0,0 +1,16 @@
+#ifndef SYSTEM_LIBRARIES_GLOBAL_H_
+#define SYSTEM_LIBRARIES_GLOBAL_H_
+
+// Macroses
+#define forever for(;;)
+#define UNUSED(arg) (void)(arg)
+
+// Typedefs
+typedef void (*func_ptr)();
+typedef unsigned char uchar;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+typedef unsigned short ushort;
+
+
+#endif /* SYSTEM_LIBRARIES_GLOBAL_H_ */
