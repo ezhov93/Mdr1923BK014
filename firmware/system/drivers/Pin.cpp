c5660cdfd83dd63884457c527a72775f8a1c2424
ezhov93 <ezhov93@users.noreply.github.com>
2020-05-20 19:40:17 +0300

    Update Pin.cpp
    add c++ cast
    change path
    edit condition form

--- firmware/system/drivers/Pin.cpp
+++ firmware/system/drivers/Pin.cpp
@@ -1,6 +1,6 @@
 #include "Pin.h"
 
-#include "1923VK014.h"
+#include "system/1923VK014.h"
 
 void Pin::init(const Mode mode, const Function function) const {
   enum Power { PowerDiverOff, Power300ns, Power100ns, Power10ns };
@@ -9,14 +9,17 @@ void Pin::init(const Mode mode, const Function function) const {
   const uint portClkEnable = (1 << (13 + ((_portAddr & 0xFFFF) >> 12)));
   CLK_CNTR->PER0_CLK |= portClkEnable;
 
-  PortControl *port = (PortControl *)(_portAddr);
+  PortControl *port = reinterpret_cast<PortControl *>(_portAddr);
   port->KEY = _KEY_;
   // Function
   const uint cntOfFunction = _number / 8;
   const uint func = (function << ((_number % 8) * 4));
   port->CFUNC[cntOfFunction] = func;
   // Input / Output
-  (mode < Output) ? port->COE : port->SOE = numberPos();
+  if (mode < Output)
+    port->COE = numberPos();
+  else
+    port->SOE = numberPos();
   // Digital / Analog
   if (mode == AnalogInput || mode == AnalogOutput)
     port->CANALOG = numberPos();
@@ -39,9 +42,13 @@ void Pin::init(const Mode mode, const Function function) const {
 }
 
 bool Pin::read() const {
-  return ((PortControl *)(_portAddr))->RXTX & numberPos();
+  return reinterpret_cast<PortControl *>(_portAddr)->RXTX & numberPos();
 }
 
-void Pin::set() const { ((PortControl *)(_portAddr))->SRXTX = numberPos(); }
+void Pin::set() const {
+  reinterpret_cast<PortControl *>(_portAddr)->SRXTX = numberPos();
+}
 
-void Pin::reset() const { ((PortControl *)(_portAddr))->CRXTX = numberPos(); }
+void Pin::reset() const {
+  reinterpret_cast<PortControl *>(_portAddr)->CRXTX = numberPos();
+}
