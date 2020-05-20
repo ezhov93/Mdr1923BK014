6558666a4217d9b6acd4f30a16638c6cbc5a16f3
ezhov93 <ezhov93@users.noreply.github.com>
2020-05-20 19:36:16 +0300

    Update Ring.h
    void read write

--- firmware/system/libraries/Ring.h
+++ firmware/system/libraries/Ring.h
@@ -5,55 +5,43 @@ template <typename T = unsigned char, int Size = 64>
 class Ring {
  public:
   constexpr Ring() : _head(0), _tail(0) {}
-
   ~Ring() = default;
-
   bool isFull() const {
     return ((_tail + 1) == _head) || ((_tail == Size) && (_head == 0));
   }
-
   bool isEmpty() const { return _head == _tail; }
-
   void push(const T data) { push(data, 1); }
-
-  void push(const T *data, int size) {
+  void push(const T* data, int size) {
     for (int cnt = 0; cnt < size; ++cnt) {
       _data[_tail] = data[cnt];
       _tail = (++_tail == Size) ? 0 : _tail;
       if (isFull()) return;
     }
   }
-
   const T pop() {
     T data;
     pop(data, 1);
     return data;
   }
-
-  void pop(T *data, int size) {
+  void pop(T* data, int size) {
     for (int cnt = 0; cnt < size; ++cnt) {
       if (isEmpty()) return;
       data[cnt] = _data[_head];
       _head = (++_head == Size) ? 0 : _head;
     }
   }
-
   const T peek() {
     T data;
     peek(&data, 1);
     return data;
   }
-
-  void peek(T *data, int size) {
+  void peek(T* data, int size) {
     const int head = _head;
     pop(data, size);
     _head = head;
   }
-
   void clear() { _head = _tail; }
-
   int size() const { return Size; }
-
   int available() const {
     int cnt = _tail - _head;
     if (cnt < 0)
@@ -61,8 +49,7 @@ class Ring {
     else
       return cnt;
   }
-
-  const void *data_ptr() const { return reinterpret_cast<void *>(_data); }
+  const void* data_ptr() const { return reinterpret_cast<void*>(_data); }
 
  private:
   T _data[Size];
